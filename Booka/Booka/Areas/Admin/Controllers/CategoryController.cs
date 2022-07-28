938ecd6478e67d17b7926552055cbe9d903e9c21
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2022-07-28 23:46:05 +0100

    Added Category Controller and project images

---
+++ Booka/Booka/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Booka.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Booka.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _db.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
---
+++ Booka/Booka/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booka.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
